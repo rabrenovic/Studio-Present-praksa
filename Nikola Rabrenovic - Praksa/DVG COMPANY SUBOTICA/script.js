df018eee9cf21ff12ced0001936f22aba77a47ff
rabrenovic <121344577+rabrenovic@users.noreply.github.com>
2023-05-08 12:20:37 +0200

    Add files via upload

--- Nikola Rabrenovic - Praksa/DVG COMPANY SUBOTICA/script.js
+++ Nikola Rabrenovic - Praksa/DVG COMPANY SUBOTICA/script.js
@@ -1,53 +1,23 @@
-const list = document.querySelector(".list");
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 30,
 
-const listItems = document.querySelectorAll(".listItem");
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-const hamburger= document.querySelector(".hamburger");
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-const closeIcon= document.querySelector(".closeIcon");
-const menuIcon = document.querySelector(".menuIcon");
-
-function toggleMenu() {
-    if (menu.classList.contains("showMenu")) {
-      menu.classList.remove("showMenu");
-      closeIcon.style.display = "none";
-      menuIcon.style.display = "block";
-    } else {
-      menu.classList.add("showMenu");
-      closeIcon.style.display = "block";
-      menuIcon.style.display = "none";
-    }
-  }
-  
-  hamburger.addEventListener("click", toggleMenu);
-  
-  listItems.forEach( 
-    function(listItem) { 
-      listItem.addEventListener("click", toggleMenu);
-    }
-  )
-
-  import Swiper from 'swiper';
-  import 'swiper/css';
-
-  const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
